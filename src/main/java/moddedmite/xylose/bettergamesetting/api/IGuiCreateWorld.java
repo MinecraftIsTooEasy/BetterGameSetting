1476119cb4877e0efa3a29fcff14ea77885824bf
Xy_Luce <155866943+ysesiq@users.noreply.github.com>
2026-06-10 14:38:57 +0800

    Conflict resolution

--- src/main/java/moddedmite/xylose/bettergamesetting/api/IGuiCreateWorld.java
+++ src/main/java/moddedmite/xylose/bettergamesetting/api/IGuiCreateWorld.java
@@ -1,6 +1,7 @@
 package moddedmite.xylose.bettergamesetting.api;
 
 import net.minecraft.GuiButton;
+import org.spongepowered.asm.mixin.Unique;
 
 import java.util.List;
 import java.util.Map;
@@ -9,4 +10,6 @@ public interface IGuiCreateWorld {
     Map<Integer, String> bgs$getHoverTexts();
     int bgs$getCurrentTab();
     List<GuiButton> bgs$getTabButtons();
+    void switchSkillsEnable();
+    boolean isSkillsEnable();
 }
